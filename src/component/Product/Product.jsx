cbf4e71624b6684feab1dbdd9beb31780f7d75e8
Masud <ranamasud0701@gmail.com>
2023-03-31 17:00:22 +0600

    font icon add

--- src/component/Product/Product.jsx
+++ src/component/Product/Product.jsx
@@ -9,13 +9,24 @@ const Product = () => {
           .then(data=>setCover(data));
      },[])
     return (
-        <div>
-            
-             {
+        <div className='container mx-auto flex justify-between gap-10 mb-10'>
+            <div>
+            {
                  cover.map(data=><Takedata 
                     key={data.id}
                     data={data}></Takedata>)
              }   
+            </div>
+            <div className=''>
+                <div className='mb-10 bg-zinc-200 p-4 rounded-lg'>
+                  Spent time on read : 0 min
+                </div>
+              
+               <div className='bg-slate-300 p-4 rounded-lg'>
+                   <h1>Bookmarked Blogs : 0</h1>
+               </div>
+            </div>
+            
         </div>
     );
 };
--- src/component/Takedata/Takedata.jsx
+++ src/component/Takedata/Takedata.jsx
@@ -1,32 +1,33 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 
 const Takedata = (props) => {
     console.log(props.data)
-    const{id,img,url,title,name,date,time}=props.data;
+    const { id, img, url, title, name, date, time } = props.data;
     return (
-        <div className='container mx-auto flex justify-between gap-10 mb-10'>
-            <div className='border border-indigo-600 rounded-lg '>
-             
-              <img className='h-96' src={img} alt="" />
-              <div className='flex justify-between mt-8'>
-              <div className='flex justify-items-center items-center gap-4'>
-               <img className='h-6 mt-3' src={url} alt="" />
-              
-               <p>{name}</p>
-               
-               
-               </div>
-               <p>{time}</p>
-              </div>
-              
-               <p className='ml-8'>{date}</p>
-               <h1 className='text-2xl font-semibold mt-10'>{title}</h1>
-               <a className='text-sky-500 mt-20' href="#">Mark as read</a>
+        <div className=''>
+            <div className='border border-indigo-600 rounded-lg mb-10 '>
+
+                <img className='h-96' src={img} alt="" />
+                <div className='flex justify-between mt-8'>
+                    <div className='flex justify-items-center items-center gap-4'>
+                        <img className='h-6 mt-3' src={url} alt="" />
+
+                        <p className='font-medium'>{name}</p>
+
+
+                    </div>
+                    <p className='pr-2 text-red-500'>{time} <FontAwesomeIcon icon={faBookmark } />
+</p>
+                </div>
+
+                <p className='ml-8'>{date}</p>
+                <h1 className='text-2xl font-semibold mt-10'>{title}</h1>
+                <a className='text-sky-500 mt-20' href="#">Mark as read</a>
             </div>
-            <div>
-                 <h1>order samary</h1>
-            </div>
-            
+
+
         </div>
     );
 };
